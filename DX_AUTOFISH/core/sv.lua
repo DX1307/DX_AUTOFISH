2861d04ede09da4af810ab42b21591d122c9bd08
DX HIGH <107764618+DX1307@users.noreply.github.com>
2023-08-28 01:13:53 +0700

    Update sv.lua

--- DX_AUTOFISH/core/sv.lua
+++ DX_AUTOFISH/core/sv.lua
@@ -1,30 +1,29 @@
-ESX = nil
+ESX = exports['es_extended']:getSharedObject()
 
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
-ESX.RegisterUsableItem(Config.Itemuse, function(source) --ชุป
+ESX.RegisterUsableItem(Config.Itemuse, function(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
     TriggerClientEvent('DX-AutoFish:USEITEM',source)
 	Citizen.Wait(10000)
 end)
 
+
+ESX.RegisterServerCallback('DX-AutoFish:getPlayerInvLimit',function(source, cb, item, amount)
+    local src = source
+    local xPlayer = ESX.GetPlayerFromId(src)
+    local invItem = xPlayer.getInventoryItem(item)
+	if (invItem.limit >= amount + invItem.count) or invItem.limit == -1 then 
+        cb(true) 
+    else 
+        cb(false) 
+    end
+end)
+
 RegisterServerEvent('DX-AutoFish:ADD')
-AddEventHandler('DX-AutoFish:ADD', function()
+AddEventHandler('DX-AutoFish:ADD', function(ItemName,ItemCount,Percent)
 	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
-	local xItem = xPlayer.getInventoryItem(Config.Additem)
-	local xZItem = xPlayer.getInventoryItem(Config.Removeitem)
-	if xItem.limit ~= -1 and (xItem.count + 1) > xItem.limit and xZItem.count <= 0 then 
-		TriggerClientEvent('DX-AutoFish:CANCLE',source)
-	else
-		xPlayer.addInventoryItem(Config.Additem, 5)
+	if math.random(1, 100) <= Percent then
+		xPlayer.addInventoryItem(ItemName, ItemCount)
 		xPlayer.removeInventoryItem(Config.Removeitem, 1)
-	end
-
-	for k,v in pairs(Config.droprate) do
-		if math.random(1, 100) <= v.Percent then
-			local xItemCount = math.random(v.ItemCount[1], v.ItemCount[2])
-			xPlayer.addInventoryItem(v.ItemName, xItemCount)
-		end
 	end
 end)
