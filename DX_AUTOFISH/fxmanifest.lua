7a3ffadbaf8dce57bd368c1b0661c55447698a94
DX HIGH <107764618+DX1307@users.noreply.github.com>
2023-08-28 01:14:23 +0700

    Update fxmanifest.lua

--- DX_AUTOFISH/fxmanifest.lua
+++ DX_AUTOFISH/fxmanifest.lua
@@ -1,6 +1,10 @@
 fx_version "adamant"
 game "gta5"
-version "1.1 FixBug"
+version "1.2 AddSystem"
+
+name "DX_AUTOFISH"
+author  "DX Development"
+repository "https://github.com/DX1307/DX_AUTOFISH.git"
 description 'Developed by DX#9190'
 
 client_scripts {
